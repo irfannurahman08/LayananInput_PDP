e2c7e4b0a3f69c3a10689e5711c7ca5517d7fe4f
irfannurahman08 <irfannurahman082gmail.com>
2020-04-28 01:46:40 +0700

    Initial commit

---
+++ UTS_SISTER_0074.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="UTS_SISTER_0074.aspx.cs" Inherits="UTS_SISTER_0074" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Input PDP Positif   &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;: 
+        <asp:TextBox ID="txtPDPPositif" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnPDPPositif" runat="server" Text="Input" OnClick="btnPDPPositif_Click" />
+        <br />
+            <br />
+        Input PDP Sembuh&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: 
+        <asp:TextBox ID="txtPDPnegatif" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnPDPnegatif" runat="server" Text="Input" OnClick="btnPDPnegatif_Click" />
+        <br />
+        <br />
+        Input PDP Meninggal : <asp:TextBox ID="txtPDPmeninggal" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnPDPmeninggal" runat="server" Text="Input" OnClick="btnPDPmeninggal_Click" />
+            <br />
+            <br />
+
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="pesan" runat="server" Text="-----"></asp:Label>
+        <br />
+
+        <br />
+
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" DataKeyNames="Id">
+        <Columns>
+             <asp:BoundField DataField="id" HeaderText="id" ItemStyle-Width="100px" />
+            <asp:TemplateField HeaderText="PDP Positif" ItemStyle-Width="150px">
+                <ItemTemplate>
+                    <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="PDP Sembuh " ItemStyle-Width="150px">
+                <ItemTemplate>
+                    <asp:Label ID="Label2" runat="server" Text=""></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="PDP Meninggal" ItemStyle-Width="150px">
+                <ItemTemplate>
+                    <asp:Label ID="Label3" runat="server" Text=""></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+        <br />
+        <asp:Label ID="total_pdp" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ UTS_SISTER_0074.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+//By. Irfan_NA0074
+public partial class UTS_SISTER_0074 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            //isi dulu dataTable
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[4] { new DataColumn("Id"), new DataColumn("pdp_positif"), 
+                new DataColumn("pdp_negatif"), new DataColumn("pdp_meniggal")});
+
+            dt.Rows.Add("Data Baru", "", "", "");
+            dt.Rows.Add("Total Data", "", "", "");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+
+    static int total_data_pdp_positif;
+    protected void btnPDPPositif_Click(object sender, EventArgs e)
+    {
+        //kirim data ke web service
+        webService.Service objek = new webService.Service();
+
+        int jmlPDP_Positif = objek.PDP_Positif(Convert.ToInt32(txtPDPPositif.Text));
+        pesan.Text = "jumlah PDP Positif : " + jmlPDP_Positif.ToString() + " Telah di input";
+
+        //method isi ke tabel
+        foreach (GridViewRow row in GridView1.Rows)
+        {
+            if (row.RowType == DataControlRowType.DataRow)
+            {
+                //input ke tabel kolom databaru pdp_positif
+                if (row.RowIndex == 0)
+                {
+                    Label lb11 = (Label)row.FindControl("Label1");
+                    lb11.Text = txtPDPPositif.Text;
+
+                }
+                //input ke tabel kolom databaru total_data_pdp_positif
+                if (row.RowIndex == 1)
+                {
+                    Label lb12 = (Label)row.FindControl("Label1");
+
+                    int data_baru = Convert.ToInt32(txtPDPPositif.Text);
+                    total_data_pdp_positif = data_baru + total_data_pdp_positif;
+                    lb12.Text = Convert.ToString(total_data_pdp_positif);
+                }
+            }
+
+        }
+        txtPDPPositif.Text = ""; //untuk clear textbox
+    }
+
+
+    static int total_data_pdp_negatif;
+    protected void btnPDPnegatif_Click(object sender, EventArgs e)
+    {
+
+        //kirim data ke web service
+        webService.Service objek = new webService.Service();
+        int jmlPDP_Sembuh = objek.PDP_Sembuh(Convert.ToInt32(txtPDPnegatif.Text));
+        pesan.Text = "jumlah PDP Sembuh : " + jmlPDP_Sembuh.ToString() + " Telah di input";
+
+        //method isi ke tabel
+        foreach (GridViewRow row in GridView1.Rows)
+        {
+            if (row.RowType == DataControlRowType.DataRow)
+            {
+                //input ke tabel kolom databaru
+                if (row.RowIndex == 0)
+                {
+                    Label lb11 = (Label)row.FindControl("Label2");
+                    lb11.Text = txtPDPnegatif.Text;
+
+                }
+                //input ke tabel kolom total_data_pdp_negatif
+                if (row.RowIndex == 1)
+                {
+                    Label lb12 = (Label)row.FindControl("Label2");
+
+                    int data_baru = Convert.ToInt32(txtPDPnegatif.Text);
+                    total_data_pdp_negatif = data_baru + total_data_pdp_negatif;
+                    lb12.Text = Convert.ToString(total_data_pdp_negatif);
+                }
+            }
+
+        }
+        txtPDPnegatif.Text = ""; //untuk clear textbox
+    }
+
+
+    static int total_data_pdp_meninggal;
+    protected void btnPDPmeninggal_Click(object sender, EventArgs e)
+    {
+
+        //kirim data ke web service
+        webService.Service objek = new webService.Service();
+        int jmlPDPmeninggal = objek.PDP_Meninggal(Convert.ToInt32(txtPDPmeninggal.Text));
+        pesan.Text = "jumlah PDP Meninggal : " + jmlPDPmeninggal.ToString() + " Telah di input";
+
+        //method isi ke tabel
+        foreach (GridViewRow row in GridView1.Rows)
+        {
+            if (row.RowType == DataControlRowType.DataRow)
+            {
+                //input ke tabel kolom databaru
+                if (row.RowIndex == 0)
+                {
+                    Label lb11 = (Label)row.FindControl("Label3");
+                    lb11.Text = txtPDPmeninggal.Text;
+
+                }
+                //input ke tabel kolom total data
+                if (row.RowIndex == 1)
+                {
+                    Label lb12 = (Label)row.FindControl("Label3");
+
+                    int data_baru = Convert.ToInt32(txtPDPmeninggal.Text);
+                    total_data_pdp_meninggal = data_baru + total_data_pdp_meninggal;
+                    lb12.Text = Convert.ToString(total_data_pdp_meninggal);
+                }
+            }
+
+        }
+        txtPDPmeninggal.Text = ""; //untuk clear textbox
+    }
+}
